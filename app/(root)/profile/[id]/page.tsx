18e07a127e31080ccd93821255b23d05f425b181
mdaashir <s.mohamedaashir@gmail.com>
2025-05-27 15:41:17 +0530

    feat: enhance ProfilePage with user video retrieval and empty state handling; add new SVG icons

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,64 +1,50 @@
-import Header from '@/components/Header';
-import VideoCard from '@/components/VideoCard';
+import { redirect } from 'next/navigation';
+import { getAllVideosByUser } from '@/lib/actions/video';
+import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import EmptyState from "@/components/EmptyState";
 
-const page = async ({ params }: ParamsWithSearch) => {
+const ProfilePage = async ({ params, searchParams }: ParamsWithSearch) => {
 	const { id } = await params;
+	const { query, filter } = await searchParams;
+
+	const { user, videos } = await getAllVideosByUser(id, query, filter);
+	if (!user) redirect('/404');
+
 	return (
-		<div className='wrapper page'>
+		<main className='wrapper page'>
 			<Header
-				subHeader='s.mohamedaashir@gmail.com'
-				title='Mohamed Aashir S | mdaashir'
-				userImg='/assets/images/dummy.jpg'
+				subHeader={user?.email}
+				title={user?.name}
+				userImg={user?.image ?? ''}
 			/>
-			<h1 className='text-2xl font-karla'> USER ID: {id} </h1>
-			<section className='video-grid'>
-				<VideoCard
-					id='1'
-					title='SnapChat Messsage'
-					thumbnail='/assets/images/video.png'
-					createdAt={new Date('2025-05-25 00:00:00.000')}
-					userImg='/assets/images/jason.png'
-					username='jason'
-					views={20}
-					visibility='public'
-					duration={156}
+
+			{videos?.length > 0 ? (
+				<section className='video-grid'>
+					{videos.map(({ video }) => (
+						<VideoCard
+							key={video.id}
+							id={video.videoId}
+							title={video.title}
+							thumbnail={video.thumbnailUrl}
+							createdAt={video.createdAt}
+							userImg={user.image ?? ''}
+							username={user.name ?? 'Guest'}
+							views={video.views}
+							visibility={video.visibility}
+							duration={video.duration}
+						/>
+					))}
+				</section>
+			) : (
+				<EmptyState
+					icon='/assets/icons/video.svg'
+					title='No Videos Available Yet'
+					description='Video will show up here once you upload them.'
 				/>
-				<VideoCard
-					id='1'
-					title='SnapChat Messsage'
-					thumbnail='/assets/images/video.png'
-					createdAt={new Date('2025-05-25 00:00:00.000')}
-					userImg='/assets/images/jason.png'
-					username='jason'
-					views={20}
-					visibility='public'
-					duration={156}
-				/>
-				<VideoCard
-					id='1'
-					title='SnapChat Messsage'
-					thumbnail='/assets/images/video.png'
-					createdAt={new Date('2025-05-25 00:00:00.000')}
-					userImg='/assets/images/jason.png'
-					username='jason'
-					views={20}
-					visibility='public'
-					duration={156}
-				/>
-				<VideoCard
-					id='1'
-					title='SnapChat Messsage'
-					thumbnail='/assets/images/video.png'
-					createdAt={new Date('2025-05-25 00:00:00.000')}
-					userImg='/assets/images/jason.png'
-					username='jason'
-					views={20}
-					visibility='public'
-					duration={156}
-				/>
-			</section>
-		</div>
+			)}
+		</main>
 	);
 };
 
-export default page;
+export default ProfilePage;
